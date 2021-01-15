0ec56ce7ae071425af43127665539a11ffe4c6b4
sansqra <sqrasan@gmail.com>
2021-01-15 13:00:48 +0530

    gets json of chat

--- content.js
+++ content.js
@@ -1,5 +1,10 @@
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     const getDivs = document.querySelectorAll('[data-sender-name]')
-    console.log(getDivs)
-    sendResponse({ chats: getDivs })
+    let innerhtmltext = {}
+    for (let i = 0; i < getDivs.length; i++) {
+
+        innerhtmltext[i] = getDivs[i].innerHTML
+    }
+    console.log(innerhtmltext)
+    // sendResponse({ chats: getDivs })
 })
